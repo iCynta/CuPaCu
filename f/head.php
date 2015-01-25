8fec94a1f4cffdfb132662df706329c90b10ca50
iCynta <rpv19852000@gmail.com>
2015-01-25 15:33:34 +0400

    test

--- f/head.php
+++ f/head.php
@@ -4,8 +4,8 @@
 <meta name="keywords" content="copacu,copy,paste,cut" />
 <meta name="description" content="A new way of sharing to the world and with your friends.Copying will save your contents,Cutting will filter and Pasting will pass the content to desired peoples" />
 <title>Copacu</title>
-<link rel="shortcut icon" href="/f/image/icon/copacu.ico" type="image/x-icon" />
-<link rel="stylesheet" href="/f/css/s.css" type="text/css" media="screen" />
+<link rel="shortcut icon" href="/image/icon/copacu.ico" type="image/x-icon" />
+<link rel="stylesheet" href="f/css/s.css" type="text/css" media="screen" />
 <link rel='stylesheet' media='screen and (max-device-width: 480px)' href='/f/css/ms.css' type='text/css' />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
